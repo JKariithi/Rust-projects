584d19c89bab2532a5d87351eff3480b15a6209f
JKariithi <kariithijanice@gmail.com>
2024-02-22 10:16:42 +0300

    Simple hello world code

---
+++ No-std/bare-metal/target/debug/bare-metal.d
@@ -0,0 +1,1 @@
+/home/jamie/Documents/rustStuff/Rust-projects/No-std/bare-metal/target/debug/bare-metal: /home/jamie/Documents/rustStuff/Rust-projects/No-std/bare-metal/src/main.rs
---
+++ No-std/bare-metal/target/debug/deps/bare_metal-839dcba12f6a03b5.d
@@ -0,0 +1,5 @@
+/home/jamie/Documents/rustStuff/Rust-projects/No-std/bare-metal/target/debug/deps/bare_metal-839dcba12f6a03b5: src/main.rs
+
+/home/jamie/Documents/rustStuff/Rust-projects/No-std/bare-metal/target/debug/deps/bare_metal-839dcba12f6a03b5.d: src/main.rs
+
+src/main.rs:
